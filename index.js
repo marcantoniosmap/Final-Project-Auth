0884e30a7a6ebd172b3416696417e64862ac0e62
marcantoniosmap <marcantoniosmap@gmail.com>
2020-06-11 19:03:02 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors= require('cors');
+
+//import routes
+const authRoute = require('./routes/auth');
+
+
+
+dotenv.config();
+
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false },()=>{
+    console.log("connected to db");
+});
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+//ROUTES
+app.get('/',(req,res)=>{
+    res.send('we are here');
+});
+
+app.use('/api/user', authRoute);
+
+
+app.listen(8000);
---
+++ routes/auth.js
@@ -0,0 +1,89 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {registerValidation,loginValidation}= require('../validation')
+
+router.post('/register', async(req,res) =>{
+
+    //validate before submit
+    const {error}= registerValidation(req.body);
+    if (error) return res.send(error.details[0].message);
+    
+    const emailExist = await User.findOne({email: req.body.email});
+    if (emailExist) return res.send('Email Already exists');
+
+
+    //HASH THE PASSWORD
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword= await bcrypt.hash(req.body.password, salt);
+
+     const user = new User({
+         name: req.body.name,
+         email: req.body.email,
+         password: hashPassword,
+     });
+     try{
+         const savedUser = await user.save();
+         res.send({status:'success',user: user._id});
+     }catch(err){
+         res.send({status:'failed',info:'error occured'});
+     }
+});
+
+router.post('/login', async (req, res)=>{
+   //validate before submit
+    const {error}= loginValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email or password is wrong');
+
+
+    //check if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('Invalid password');
+
+    
+    //Create token
+    const token = jwt.sign({_id: user._id},process.env.TOKEN_SECRET,{expiresIn:129600});
+    res.header('auth-token', token);
+    res.send({token:token,id:user._id});
+});
+
+router.get('/checkToken/:token',async(req,res)=>{
+    const token = req.params.token;
+    if(!token) res.status(401).send('Access Denied');
+    else{
+        try{
+            const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+            const expired=verified.exp;
+
+            if (Date.now()>= expired*1000){
+                res.status(403).send('Expired Token');
+            }else{
+                req.user= verified;
+                res.status(200).send(req.user);
+            }
+    
+        } catch (err){
+            res.status(403).send('Invalid Token');
+        }
+    }
+  
+
+});
+
+router.get('/',async (req,res)=>{
+    // const user = await User.findById(req.params.id);
+    // const userRet={
+    //     _id:user._id,   
+    //     name:user.name,
+    //     email:user.email,
+    // }
+    res.send({test:"success"});
+});
+
+
+
+module.exports= router;
