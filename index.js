32bbded68b5c53148d2f42b43a2fa959b70c93b9
amartya <amartya.sk@gmail.com>
2020-06-12 02:53:17 +0700

    fixed jest testing

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-const express = require('express');
-const app = express();
-const router = require('express').Router();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors= require('cors');
+const app = require('./server');
 
-//import routes
-const authRoute = require('./routes/auth');
-
-dotenv.config();
-
-
-//Connect to DB
-mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify: false },()=>{
-    console.log("connected to db");
-});
-
-//middlewares
-app.use(express.json());
-app.use(cors());
-
-
-//ROUTES
-app.get('/',(req,res)=>{
-    res.send('we are here');
-});
-
-app.use('/api/user', authRoute);
-
-
-const server = app.listen(8000);
-server.close();
-
-module.exports=app
+app.listen(8000);
