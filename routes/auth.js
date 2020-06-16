3d3f5c96b024d5ad8af049b28238a476552a2a80
marcantoniosmap <marcantoniosmap@gmail.com>
2020-06-16 22:16:09 +0700

    user profile api done

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const {registerValidation,loginValidation}= require('../validation')
 const verify = require('./verifyToken');
+const UserProject = require('../model/UserProject');
+const fetch = require('node-fetch');
+
+
+const domainUrl= 'https://project.cogether.me';
+// const domainUrl = 'https://localhost:9000'
+const deleteUrl = domainUrl+'/delete/'
 
 
 router.post('/register', async(req,res) =>{
@@ -78,13 +85,97 @@ router.get('/checkToken/:token',async(req,res)=>{
 
 });
 
+
+router.put('/update',verify,async(req,res)=>{
+    const user = await User.findOne({_id:req.body._id});
+    if (user.id===req.user._id){
+        const updateUser = await User.updateOne(
+            {_id:user.id},
+            {$set:{name:req.body.name}}
+        );
+        res.status(200).send({status:"OK"})
+    } else{
+        res.status(400).send({err:"cannot update name"});
+    }
+});
+
+router.put('/updatePassword',verify,async(req,res)=>{
+    try{
+        const user = await User.findOne({_id:req.body._id});
+        if (user.id===req.user._id){
+        const validPass = await bcrypt.compare(req.body.password, user.password);
+        if (!validPass) return res.status(400).send({err:'unable to change password'});
+
+        //hash
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword= await bcrypt.hash(req.body.newPassword, salt);
+        const updateUser = await User.updateOne(
+            {_id:user.id},
+            {$set:{password:hashPassword}}
+        );
+        res.status(200).send({status:"OK"})
+    } else{
+        res.status(400).send({err:"cannot change password"});
+    }
+}catch(err){
+    res.status(400).send({err:err});
+}
+});
+
+const deleteProject =async (project_id,token)=>{
+    try{
+      const deletePrj = await fetch(deleteUrl+project_id,
+        {method:'DELETE', headers:{
+          "Content-Type":'application/json',
+          "auth-token":token
+        }, body:JSON.stringify({
+          idList:idList})});
+          const json = await deletePrj.json();
+          return json;
+    } catch (err){
+        return(err);
+    }
+
+}
+
+router.delete('/deleteUser/:id',verify,async(req,res)=>{
+    try{
+        const user = await User.findOne({_id:req.params.id});
+        if(user.id=== req.user._id){
+            const projects = await UserProject.find({user_id:user.id});
+            // const projectCOllaborated = await UserProject.find({user_id:user.id,ownership_type:"collaborator"});
+            const deletedUser = await User.deleteOne({_id:req.params.id});
+            for(project of projects){
+                if (project.ownership_type==='owner'){
+                    try{
+                        const deleteProjects = await deleteProject(project._id,req.header('auth-token'));
+                    }catch(err){
+                        console.log(err);
+                    }
+                }else{
+                    const deleteAsCollaborator = await UserProject.delete({project_id:project.project_id,user_id:user.id});
+                }
+            }
+            res.status(400).send({status:'OK'});
+        }
+    }catch(err){
+        res.status(400).send({err:err});
+    }
+})
+
 router.get('/:id',verify,async(req,res)=>{
+    if (req.user._id !== req.params.id) return res.status(400).send({err:'unauthorized'});
    try{
     const user = await User.findOne({_id:req.params.id});
+    const projectOwned = await UserProject.find({user_id:req.params.id,ownership_type:"owner"});
+    const projectCollab = await UserProject.find({user_id:req.params.id,ownership_type:"collaborator"});
     const retUser={
         _id: user._id,
         name: user.name,
-        email:user.email
+        email:user.email,
+        project_owner: projectOwned.length,
+        project_collab : projectCollab.length,
+
     };
     res.status(200).send(retUser);
    }catch(err){
